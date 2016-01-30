685e75a617e2afd0f285e4f74ea9c0877a65685f
hecferme <hecferme@gmail.com>
2016-01-30 10:04:01 -0600

    se incluye wcf para Hello World y pruebas automatizadas

---
+++ Colaborativos.Warming/Hello.World.Tests/UnitTest1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Hello.World.Tests.Client;
+
+namespace Hello.World.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestSayHelloMyName ()
+        {
+            // preparar el escenario
+            string myName = "Héctor";
+            string resultadoEsperado = "Hello World, Héctor";
+            string resultadoReal;
+            HelloWorldClient client = new HelloWorldClient();
+
+            // invoco al método correspondiente
+            resultadoReal = client.SayHello(myName);
+
+            // comparación de resultados
+            Assert.AreEqual(resultadoEsperado, resultadoReal);
+
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
